2cab435391afe3f1228a6abb5c726070250b7b1a
AlexanderV <volodin@cmd.su>
2026-09-16 13:43:24 +0300

    refactor: перевод на HTTP

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 	"net/http"
 	delivery "webcam-streamer/delivery/http"
 	"webcam-streamer/infrastructure/v4l2"
@@ -10,26 +11,22 @@ import (
 )
 
 func main() {
-	// 1. Инициализируем инфраструктуру (драйверы)
 	cameraRepo := v4l2.NewV4L2Repository()
 	cameraStreamer := v4l2.NewV4L2Streamer()
-
-	// 2. Инициализируем бизнес-логику (Usecase), внедряя инфраструктуру
 	cameraUC := usecase.NewCameraUseCase(cameraRepo, cameraStreamer)
-
-	// 3. Инициализируем транспортный слой (Delivery), внедряя Usecase
 	httpHandler := delivery.NewHTTPHandler(cameraUC)
 
-	// 4. Настраиваем маршрутизатор
 	mux := http.NewServeMux()
 	httpHandler.RegisterRoutes(mux)
 
-	// 5. Запуск HTTPS
-	certFile := "server.crt"
-	keyFile := "server.key"
+	// Читаем порт из окружения Docker
+	port := os.Getenv("APP_PORT")
+	if port == "" {
+		port = "8080"
+	}
 
-	fmt.Println("Легковесный стример (Clear Architecture) запущен на https://localhost:8443")
-	if err := http.ListenAndServeTLS(":8443", certFile, keyFile, mux); err != nil {
+	fmt.Printf("Легковесный стример запущен внутри контейнера на http://localhost:%s\n", port)
+	if err := http.ListenAndServe(":"+port, mux); err != nil {
 		log.Fatalf("Ошибка сервера: %v", err)
 	}
 }
